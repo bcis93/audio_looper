408b29986ecc4d626f7c384f0d4927a758f315cd
bcis93 <bryancisneros93@gmail.com>
2019-11-02 11:04:59 -0600

    reverting previous change and initializing track length to 0

--- Globals.py
+++ Globals.py
@@ -1,5 +1,5 @@
 recording_mode = True # true if recording mode, false if playing mode
 master_done = False # true every time the master track starts over. will stay true only for one tick cycle
 waiting_to_start = 0 # 0 until the first track starts recording, 1 while the first track is recording, 2 once the first track finishes
-track_length = 176400
+track_length = 0
 audio_wrap_around = False
