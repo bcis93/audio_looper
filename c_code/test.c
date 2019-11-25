33e81cb4a9b50682a44b5dfb560313fe8bf15b7f
bcis93 <bryancisneros93@gmail.com>
2019-11-25 23:50:53 +0000

    working with fe-pi

--- c_code/test.c
+++ c_code/test.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <bcm2835.h>
+#include <sched.h>
 
 // gcc test.c -lrt -lasound -lpthread -lportaudio -o test
 
@@ -12,16 +14,25 @@ typedef struct
 }   
 paTestData;
 
+#define PIN (23)
+#define RESET_PIN (12)
+
 
 #define SAMPLE_RATE (44100)
-#define SECONDS (10)
+#define SECONDS (20)
 #define AUDIO_LENGTH (441000)
+#define CHUNK_SIZE (512)
 static paTestData data;
 
 static int32_t audio[AUDIO_LENGTH] = {};
-static int current_position = 0;
+static volatile long long current_position = 0;
+static volatile long long write_position = 0;
 static volatile int count = 0;
 static volatile bool flag = false;
+static bool recording = false;
+static volatile int first_recording = 0;
+static volatile long long track_length = AUDIO_LENGTH;
+static bool reset_flag = false;
 
 /* This routine will be called by the PortAudio engine when audio is needed.
    It may called at interrupt level on some machines so don't do anything
@@ -37,17 +48,54 @@ static int patestCallback( const void *inputBuffer, void *outputBuffer,
     int32_t* out = (int32_t*) outputBuffer;
     unsigned int i;
     //outputBuffer = (void*)&(audio[current_position]);
+	//printf("tick\r\n");
+
+    if (recording && first_recording == 0)
+    {
+        first_recording++;
+        current_position = 0;
+        write_position = 0;
+    }
+    else
+    {
+        write_position = current_position - framesPerBuffer;// * 2;
+        if (write_position < 0)
+        {
+            write_position += track_length;
+        }
+    }
+    
+
     for( i=0; i<framesPerBuffer; i++ )
     {
-        //*out = *in;
-        audio[current_position] += *in;
-        *out = audio[current_position];
+        if (recording)
+        {
+            audio[write_position] += *in;
+            *out = audio[current_position] + *in;
+        }
+        else
+        {
+            *out = audio[current_position] +  *in;
+        }
+
+        // *out = *in;
+
+        // audio[current_position] += *in;
+        // *out = audio[current_position];
+
         current_position++;
-        if (current_position >= AUDIO_LENGTH)
+        if (current_position >= track_length || current_position >= AUDIO_LENGTH)
         {
             current_position = 0;
-            count++;
-            flag = true;
+            //count++;
+            //flag = true;
+        }
+        write_position++;
+        if (write_position >= track_length || write_position >= AUDIO_LENGTH)
+        {
+            write_position = 0;
+            //count++;
+            //flag = true;
         }
 
         out++;
@@ -56,8 +104,53 @@ static int patestCallback( const void *inputBuffer, void *outputBuffer,
     return 0;
 }
 
+void delay_loop_ms(int ms)
+{
+    static volatile int i;
+    static volatile int j;
+    for (i = 0; i < ms; i++)
+    {
+        for(j=0;j<100000;j++){};
+    }
+}
+
+void wait_for_press(void)
+{
+    int track1 = 1;
+    int reset = 1;
+    while(track1 && reset)
+    {
+        track1 = bcm2835_gpio_lev(PIN);
+        reset = bcm2835_gpio_lev(RESET_PIN);
+    }
+    printf("press!\n");
+    if (!reset)
+    {
+        reset_flag = true;
+    }
+}
+
 int main()
 {
+    const struct sched_param priority = {1};
+    sched_setscheduler(0, SCHED_FIFO, &priority);
+
+    if (!bcm2835_init())
+    {
+        printf("bcm2835 init failed!\n");
+        return 1;
+    }
+        
+    // Set RPI pin 23 to be an input
+    bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_INPT);
+    //  with a pullup
+    bcm2835_gpio_set_pud(PIN, BCM2835_GPIO_PUD_UP);
+
+    // Set RPI pin 23 to be an input
+    bcm2835_gpio_fsel(RESET_PIN, BCM2835_GPIO_FSEL_INPT);
+    //  with a pullup
+    bcm2835_gpio_set_pud(RESET_PIN, BCM2835_GPIO_PUD_UP);
+
     PaError err = Pa_Initialize();
     if( err != paNoError )
     {
@@ -79,7 +172,15 @@ int main()
             for( i=0; i<numDevices; i++ )
             {
                 deviceInfo = Pa_GetDeviceInfo( i );
-                printf("%s\r\n", deviceInfo->name);
+                printf("name: %s\r\n", deviceInfo->name);
+		printf("hostApi: %d\r\n", deviceInfo->hostApi);
+		printf("maxInputChannels: %d\r\n", deviceInfo->maxInputChannels);
+		printf("maxOutputChannels: %d\r\n", deviceInfo->maxOutputChannels);
+		printf("defaultLowInputLatency: %f\r\n", deviceInfo->defaultLowInputLatency);
+		printf("defaultLowOutputLatency: %f\r\n", deviceInfo->defaultLowOutputLatency);
+		printf("defaultHighInputLatency: %f\r\n", deviceInfo->defaultHighInputLatency);
+		printf("defaultHighOutputLatency: %f\r\n", deviceInfo->defaultHighOutputLatency);
+		printf("defaultSampleRate: %f\r\n\n", deviceInfo->defaultSampleRate);		
             }
         }
         
@@ -88,8 +189,8 @@ int main()
         PaStream *stream;
         PaError err;
         PaDeviceIndex index = 2;
-        PaStreamParameters input = {2, 2, paInt16, (Pa_GetDeviceInfo(2))->defaultLowInputLatency, NULL};
-        PaStreamParameters output = {2, 2, paInt16, (Pa_GetDeviceInfo(2))->defaultLowOutputLatency, NULL};
+        PaStreamParameters input = {2, 1, paInt16, (Pa_GetDeviceInfo(2))->defaultLowInputLatency, NULL};
+        PaStreamParameters output = {2, 1, paInt16, (Pa_GetDeviceInfo(2))->defaultLowOutputLatency, NULL};
 
         /* Open an audio I/O stream. */
         // err = Pa_OpenDefaultStream( &stream,
@@ -113,7 +214,7 @@ int main()
                                     &input,
                                     &output,
                                     SAMPLE_RATE,
-                                    2048,
+                                    CHUNK_SIZE,
                                     paNoFlag,
                                     patestCallback,
                                     &data );	
@@ -124,13 +225,16 @@ int main()
 
         /* Sleep for several seconds. */
         //Pa_Sleep(5*1000);
-        while (count < 5)
+        while (!reset_flag)
         {
-            if (flag)
+            wait_for_press();
+            if (first_recording == 1)
             {
-                printf("wrap\r\n");
-                flag = false;
+                track_length = current_position;
+                first_recording++;
             }
+            recording = !recording;
+            delay_loop_ms(500);
         }
 
         err = Pa_StopStream( stream );
